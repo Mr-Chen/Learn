42fb3c534d81fac45a5d41e1c7856211a40aa00c
小小实习生 <836753757@qq.com>
2022-04-21 07:07:53 +0800

    git pull --rebase 解决冲突

--- git/src/main/java/com/imooc/git/MainActivity.java
+++ git/src/main/java/com/imooc/git/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         git();
+        gitDev();
     }
 
     /**
@@ -18,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
      */
     private void git() {
         String master = "master";
+    }
+
+    /* dev 分支
+     */
+    private void gitDev() {
+
+        String dev = "dev";
     }
 }
