4e51c9038d652c1c88859161102a0dacab1a1eb2
小小实习生 <836753757@qq.com>
2022-04-21 08:48:26 +0800

    第一次提交

--- git/src/main/java/com/imooc/git/GitReset.java
+++ git/src/main/java/com/imooc/git/GitReset.java
@@ -3,7 +3,15 @@ package com.imooc.git;
 /**
  * Author   ： cxw
  * Date     ： 2022/4/21 08:40
- * Explain  :  请在此输入文件说明
+ * Explain  :  回滚
+ * git status 查看状态
+ * git log 查看提交日志
+ *
+ *  git add --all  添加所有工作区文件到暂存区
+ *  git commit -m "说明" 将暂存区的文件提交到本地仓库
+ * git log -5 --pretty=oneline  log输出格式化
+ * git reset --hard HEAD^ ^代表上一个版本可以添加多个
+ *
  */
 public class GitReset {
     String one = "第一个版本";
