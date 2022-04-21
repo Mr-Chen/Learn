9cbd87e8c5f0f77fe4e3305747f51f41eb72b35d
小小实习生 <836753757@qq.com>
2022-04-21 11:46:33 +0800

    GitTag

--- git/src/main/java/com/imooc/git/GitTag.java
+++ git/src/main/java/com/imooc/git/GitTag.java
@@ -3,7 +3,15 @@ package com.imooc.git;
 /**
  * Author   ： cxw
  * Date     ： 2022/4/21 11:31
- * Explain  :  请在此输入文件说明
+ * Explain  :  标签
+ *
+ * git tag 查看所有标签
+ * git tag v1.0 新建标签 默认为HEAD
+ * git tag -a v1.0 -m "标签描述信息" 新建标签并添加描述信息
+ * git tag -d v1.0 删除本地标签
+ * git push origin v1.0 推送本地标签到远程
+ * git push origin --tags 推送全部未推送的本地标签到远程
+ * git push origin :refs/tags/v1.0 删除远程标签
  */
 public class GitTag {
 }
