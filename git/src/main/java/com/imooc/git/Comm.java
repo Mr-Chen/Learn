0a51a643562cae2a53233f35f0a4776dd1c68de4
小小实习生 <836753757@qq.com>
2022-04-21 09:25:02 +0800

    通用æ命令

---
+++ git/src/main/java/com/imooc/git/Comm.java
@@ -0,0 +1,18 @@
+package com.imooc.git;
+
+/**
+ * Author   ： cxw
+ * Date     ： 2022/4/21 09:23
+ * Explain  :  通用命令
+ *  git status 查看状态
+ *  git log 查看提交日志
+ *  git log -5 --pretty=oneline  log输出格式化
+ *  git reflog 查看被回滚过的日志，可以恢复错误回滚
+ *  gitk& 日志工具
+ *
+ *   git add --all  添加所有工作区文件到暂存区
+ *   git commit -m "说明" 将暂存区的文件提交到本地仓库
+ *   git push 推送到远程仓库
+ */
+public class Comm {
+}
