74b1cb546d46021e82bcfb8c96da136830443220
小小实习生 <836753757@qq.com>
2022-04-21 09:14:14 +0800

    回滚

--- git/src/main/java/com/imooc/git/GitReset.java
+++ git/src/main/java/com/imooc/git/GitReset.java
@@ -8,6 +8,7 @@ package com.imooc.git;
  * git status 查看状态
  * git log 查看提交日志
  * git log -5 --pretty=oneline  log输出格式化
+ * git reflog 查看被回滚过的日志，可以恢复错误回滚
  * gitk& 日志工具
  *
  *  git add --all  添加所有工作区文件到暂存区
