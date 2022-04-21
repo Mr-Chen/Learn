1742d08e67f6550d46cd46fb4782e917ca492379
小小实习生 <836753757@qq.com>
2022-04-21 10:07:55 +0800

    GitBranch.jva

---
+++ git/src/main/java/com/imooc/git/GitBranch.java
@@ -0,0 +1,20 @@
+package com.imooc.git;
+
+/**
+ * Author   ： cxw
+ * Date     ： 2022/4/21 09:52
+ * Explain  :  分支
+ *
+ * git branch 查看所有分支 *标记当前所在分支
+ * git branch dev 新建分支
+ * git checkout -b dev 新建分支并切换到该分支
+ * git branch -d dev 删除分支
+ * git checkout dev 切换分支
+ * git merge dev 合并分支（切换到主分支，在主分支合并）
+ * git branch -m dev 重命名
+ * git branch -m | M oldName newName 如果名字已经存在强制重命名
+ *
+ *
+ */
+public class GitBranch {
+}
